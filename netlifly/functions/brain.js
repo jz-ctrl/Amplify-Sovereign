e9dba46d7e9e4710b3794953293e2372f0cc7282
jz-ctrl <jz@votejz.org>
2026-02-17 14:03:06 -0800

    Update brain.js

--- netlifly/functions/brain.js
+++ netlifly/functions/brain.js
@@ -1,10 +1,11 @@
 const GITHUB_TOKEN = process.env.GH_TOKEN;
 const SHOPIFY_KEY = process.env.SHOPIFY_API_KEY;
 
+// Log initialization for terminal confirmation
 console.log("BRAIN INITIALIZED");
 
-if (!GITHUB_TOKEN || !SHOPIFY_KEY) {
-    console.log("STATUS: RUNNING LOCAL - MISSING KEYS");
-} else {
-    console.log("STATUS: CLOUD CONNECTED");
-}
+// Exporting variables for the Netlify functions to consume
+module.exports = {
+    GITHUB_TOKEN,
+    SHOPIFY_KEY
+};
